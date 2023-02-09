cb029c5d0b30f3fc4692217ba9e6db23bf965e53
Woody Chang <woody.chang@storm.mg>
2023-02-09 09:56:52 +0800

    add export emits type

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,35 @@
       ref="editorEl"
       v-bind:set-options="currentSetup"
       v-bind:disable="disable"
-      v-on:input="onScroll"
-      v-on:save="onScroll"
-      v-on:load="onScroll"
+      v-on:audio-upload="handleAudioUpload"
+      v-on:audio-upload-before="audioUploadBefore"
+      v-on:audio-upload-error="audioUploadError"
+      v-on:blur="handleBlur"
+      v-on:input="handleInput"
+      v-on:change="handleChange"
+      v-on:click="handleClick"
+      v-on:copy="handleCopy"
+      v-on:cut="handleCut"
+      v-on:drop="handleDrop"
+      v-on:focus="handleFocus"
+      v-on:get-sun-editor-instance="handleGetSunEditorInstance"
+      v-on:image-upload="handleImageUpload"
+      v-on:image-upload-before="handleImageUploadBefore"
+      v-on:image-upload-error="handleImageUploadError"
+      v-on:image-upload-handler="handleImageUploadHandler"
+      v-on:key-down="handleKeyDown"
+      v-on:past="handlePaste"
+      v-on:resize-editor="handleResizeEditor"
+      v-on:save="handleSave"
+      v-on:scroll="handleScroll"
+      v-on:set-tool-bar-buttons="handleSetToolbarButtons"
+      v-on:show-controller="handleShowController"
+      v-on:show-inline="handleShowInline"
+      v-on:toggle-full-screen="handleToggleFullScreen"
+      v-on:video-upload="handleVideoUpload"
+      v-on:video-upload-before="handleVideoUploadBefore"
+      v-on:video-upload-error="handleVideoUploadError"
+      v-on:video-upload-handler="handleVideoUploadHandler"
     />
     <button type="button" v-on:click="changeSetup">change setup</button>
     <button type="button" v-on:click="toggleDisable">toggle disable</button>
@@ -18,27 +44,8 @@
 import plugins from 'suneditor/src/plugins';
 import { ref } from 'vue';
 
-import SunEditor, { type IEmits } from '@/SunEditor.vue';
-import type { OverloadFunc, OverloadFunc2, SetOptions } from '@/types';
-
-// const editorPlugins = {
-//   Gallery: {
-//     action: () => {},
-//     add: function (core, targetElement) {},
-//     display: 'command',
-//     innerHTML:
-//       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15.75 15.77"><g><path d="M8.77,8.72a.88.88,0,0,1-.61-.27.82.82,0,0,1-.25-.61.89.89,0,0,1,.25-.62A.82.82,0,0,1,8.77,7a.81.81,0,0,1,.61.25.83.83,0,0,1,.27.62.81.81,0,0,1-.25.61.91.91,0,0,1-.63.27Zm9.62-5a1.74,1.74,0,0,1,1.76,1.76V17.76a1.74,1.74,0,0,1-1.76,1.76H6.16A1.74,1.74,0,0,1,4.4,17.76V5.51A1.74,1.74,0,0,1,6.16,3.75H18.39Zm0,1.75H6.16v8L8.53,11.8a.94.94,0,0,1,.54-.17.86.86,0,0,1,.54.2L11.09,13l3.64-4.55a.78.78,0,0,1,.34-.25.85.85,0,0,1,.42-.07.89.89,0,0,1,.39.12.78.78,0,0,1,.28.29l2.24,3.67V5.51Zm0,12.24V15.6L15.3,10.53,11.89,14.8a.89.89,0,0,1-.59.32.82.82,0,0,1-.64-.18L9,13.62,6.16,15.74v2Z" transform="translate(-4.4 -3.75)"></path></g></svg>',
-//     name: 'Gallery',
-//   },
-//   ParagraphStyle: {
-//     action: () => {},
-//     add: function (core, targetElement) {},
-//     display: 'command',
-//     innerHTML:
-//       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11.81 15.74"><g><path d="M18.18,3.76v2h-2V19.5h-2V5.73h-2V19.5h-2V11.63a3.94,3.94,0,0,1,0-7.87h7.87Z" transform="translate(-6.37 -3.76)"></path></g></svg>',
-//     name: 'ParagraphStyle',
-//   },
-// };
+import SunEditor from '@/SunEditor.vue';
+import type { ExportIEmits, SetOptions } from '@/types';
 
 const editorConfig: SetOptions = {
   buttonList: [
@@ -98,10 +105,6 @@ const toggleDisable = () => {
   disable.value = !disable.value;
 };
 
-const onScroll = (event: Event) => {
-  console.log('event', event);
-};
-
 const handleInsert = () => {
   console.log('editorEl.value', editorEl.value);
   if (editorEl.value) {
@@ -111,23 +114,132 @@ const handleInsert = () => {
   }
 };
 
-// const selectedImage = (param) => {
-//   editor.insertHTML(
-//     `<img src="${param.image.imageURL}" alt="${param.image.title}" image-id="${param.image.id}" />
-//       <p class="mt-2 text-smg-typography-body-2 text-smg-gray-700">${param.image.title}</p>`,
-//     true,
-//   );
-// };
+// * event handlers
+const handleAudioUpload: ExportIEmits['audioUpload'] = (targetElement, index, state, info, remainingFilesCount) => {
+  console.log('audioUpload');
+};
 
-// const callback = (param) => {
-//   console.log(param);
-//   editor.insertHTML(
-//     `<p>...</p><div class='template' data-template-id='${param.data.id}'>${param.data.title}</div><p>...</p>`,
-//     true,
-//   );
-// };
-const handlers: OverloadFunc2['change'] = (content) => {
-  console.log('test');
+const audioUploadBefore: ExportIEmits['audioUploadBefore'] = (files, audioInputInformation, uploadHandler) => {
+  console.log('audioUploadBefore');
+  return false;
+};
+
+const audioUploadError: ExportIEmits['audioUploadError'] = (errorMessage, result) => {
+  console.log('audioUploadError');
+  return false;
+};
+
+const handleBlur: ExportIEmits['blur'] = (focusEvent) => {
+  console.log('blur');
+};
+
+const handleChange: ExportIEmits['change'] = (content) => {
+  console.log('change');
+};
+
+const handleClick: ExportIEmits['click'] = (mouseEvent) => {
+  console.log('click');
+};
+
+const handleCopy: ExportIEmits['copy'] = (clipboardEvent, clipboardData) => {
+  console.log('copy');
+  return false;
+};
+
+const handleCut: ExportIEmits['cut'] = (clipboardEvent, clipboardData) => {
+  console.log('cut');
+  return false;
+};
+
+const handleDrop: ExportIEmits['drop'] = (dragEvent, cleanData, maxCharCount, core) => {
+  console.log('drop');
+  return '';
+};
+
+const handleFocus: ExportIEmits['focus'] = (focusEvent) => {
+  console.log('focus');
+};
+
+const handleGetSunEditorInstance: ExportIEmits['getSunEditorInstance'] = (sunEditor) => {
+  console.log('getSunEditorInstance');
+};
+
+const handleImageUpload: ExportIEmits['imageUpload'] = (targetImgElement, index, state, info, remainingFileCount) => {
+  console.log('imageUpload');
+};
+
+const handleImageUploadBefore: ExportIEmits['imageUploadBefore'] = (files, info, core, uploadHandler) => {
+  console.log('imageUploadBefore');
+  return false;
+};
+
+const handleImageUploadError: ExportIEmits['imageUploadError'] = (errorMessage, result) => {
+  console.log('imageUploadError');
+  return false;
+};
+
+const handleImageUploadHandler: ExportIEmits['imageUploadHandler'] = (xmlHttpRequest, info, core) => {
+  console.log('imageUploadHandler');
+};
+
+const handleInput: ExportIEmits['input'] = (inputEvent) => {
+  console.log('input');
+};
+
+const handleKeyDown: ExportIEmits['keyDown'] = (keyboardEvent) => {
+  console.log('keydown');
+};
+
+const handlePaste: ExportIEmits['paste'] = (clipboardEvent, cleanData, maxCharCount, core) => {
+  console.log('paste');
+  return false;
+};
+
+const handleResizeEditor: ExportIEmits['resizeEditor'] = (height, core, prevHeight, resizeObserverEntry) => {
+  console.log('resizeEditor');
+  return {};
+};
+
+const handleSave: ExportIEmits['save'] = (contents) => {
+  console.log('save');
+};
+
+const handleScroll: ExportIEmits['scroll'] = (uiEvent) => {
+  console.log('scroll');
+};
+
+const handleSetToolbarButtons: ExportIEmits['setToolbarButtons'] = (buttonList) => {
+  console.log('setToolbarButtons');
+};
+
+const handleShowController: ExportIEmits['showController'] = (controllers, name) => {
+  console.log('showController');
+};
+
+const handleShowInline: ExportIEmits['showInline'] = (context, toolbar) => {
+  console.log('showInline');
+};
+
+const handleToggleFullScreen: ExportIEmits['toggleFullScreen'] = (isFullScreen) => {
+  console.log('toggleFullScreen');
+};
+
+const handleVideoUpload: ExportIEmits['videoUpload'] = (targetElement, index, state, info, remainingFilesCount) => {
+  console.log('videoUpload');
+};
+
+const handleVideoUploadBefore: ExportIEmits['videoUploadBefore'] = (files, info, core, uploadHandler) => {
+  console.log('videoUploadBefore');
+  return false;
+};
+
+const handleVideoUploadError: ExportIEmits['videoUploadError'] = (errorMessage, result) => {
+  console.log('videoUploadError');
+  return false;
+};
+
+const handleVideoUploadHandler: ExportIEmits['videoUploadHandler'] = (xmlHttpRequest, info, core) => {
+  console.log('videoUploadHandler');
 };
 </script>
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -25,128 +25,202 @@ export interface SetOptions extends SunEditorOptions {
   customPlugins?: Array<Plugin> | Record<string, Plugin>;
 }
 
-// export interface IProps {
-//   appendContents?: string;
-//   autoFocus?: boolean;
-//   defaultValue?: string;
-//   disable?: boolean;
-//   disableToolbar?: boolean;
-//   disableWysiwyg?: boolean;
-//   height?: string;
-//   hide?: boolean; // TODO: no need?
-//   hideToolbar?: boolean;
-//   isNoticeOpen?: boolean;
-//   lang?: LangType;
-//   name?: string;
-//   noticeMessage?: string;
-//   placeholder?: string;
-//   readOnly?: boolean;
-//   setAllPlugins?: boolean;
-//   setContents?: string;
-//   setDefaultStyle?: string;
-//   setOptions?: SetOptions;
-//   width?: string;
-// }
+// TODO: waiting for enabling to move outside of SFC, until Vue 3.3 release
+export interface IProps {
+  appendContents?: string;
+  autoFocus?: boolean;
+  defaultValue?: string;
+  disable?: boolean;
+  disableToolbar?: boolean;
+  disableWysiwyg?: boolean;
+  height?: string;
+  hide?: boolean; // TODO: no need?
+  hideToolbar?: boolean;
+  isNoticeOpen?: boolean;
+  lang?: LangType;
+  name?: string;
+  noticeMessage?: string;
+  placeholder?: string;
+  readOnly?: boolean;
+  setAllPlugins?: boolean;
+  setContents?: string;
+  setDefaultStyle?: string;
+  setOptions?: SetOptions;
+  width?: string;
+}
 
-// export interface IEmits {
-//   (event: 'change', content: string): void;
-//   (event: 'input', inputEvent: InputEvent): void;
-//   (event: 'scroll', uiEvent: UIEvent): void;
-//   (event: 'copy', clipboardEvent: ClipboardEvent, clipboardData: any): boolean;
-//   (event: 'cut', clipboardEvent: ClipboardEvent, clipboardData: any): boolean;
-//   (event: 'click', mouseEvent: PointerEvent): void;
-//   (event: 'mouseDown', mouseEvent: MouseEvent): void;
-//   (event: 'keyUp', keyboardEvent: KeyboardEvent): void;
-//   (event: 'keyDown', keyboardEvent: KeyboardEvent): void;
-//   (event: 'focus', focusEvent: FocusEvent): void;
-//   (event: 'blur', focusEvent: FocusEvent): void;
-//   (event: 'save', contents: string): void;
-//   (event: 'setToolbarButtons', buttonList: Array<any>): void;
-//   (event: 'load', reload: boolean): void;
-//   (event: 'drop', dragEvent: DragEvent, cleanData: string, maxCharCount: boolean, core: Core): boolean | string;
-//   (event: 'paste', clipboardEvent: ClipboardEvent, cleanData: string, maxCharCount: boolean, core: Core):
-//     | boolean
-//     | string;
-//   (
-//     event: 'imageUpload',
-//     targetImgElement: HTMLImageElement,
-//     index: number,
-//     state: UploadStateType,
-//     info: fileInfo,
-//     remainingFilesCount: number,
-//   ): void;
-//   (
-//     event: 'videoUpload',
-//     targetElement: HTMLElement,
-//     index: number,
-//     state: UploadStateType,
-//     info: fileInfo,
-//     remainingFilesCount: number,
-//   ): void;
-//   (
-//     event: 'audioUpload',
-//     targetElement: HTMLElement,
-//     index: number,
-//     state: UploadStateType,
-//     info: fileInfo,
-//     remainingFilesCount: number,
-//   ): void;
-//   (
-//     event: 'imageUploadBefore',
-//     files: Array<File>, // TODO: different with origin
-//     info: imageInputInformation,
-//     core: Core,
-//     uploadHandler: Function,
-//   ): boolean | any[] | undefined;
-//   (
-//     event: 'videoUploadBefore',
-//     files: Array<File>, // TODO: different with origin
-//     info: videoInputInformation,
-//     core: Core,
-//     uploadHandler: Function,
-//   ): boolean | any[] | undefined;
-//   (
-//     event: 'audioUploadBefore',
-//     files: Array<File>, // TODO: different with origin
-//     info: audioInputInformation,
-//     uploadHandler: Function,
-//   ): boolean | any[] | undefined;
-//   (event: 'imageUploadError', errorMessage: string, result: any): boolean;
-//   (event: 'videoUploadError', errorMessage: string, result: any): boolean;
-//   (event: 'audioUploadError', errorMessage: string, result: any): boolean;
-//   (event: 'toggleCodeView', isCodeView: boolean): void;
-//   (event: 'toggleFullScreen', isFullScreen: boolean): void;
-//   (event: 'showInline', context: Context, toolbar: Element): void;
-//   (event: 'showController', controllers: Controllers, name: string): void;
-//   (event: 'imageUploadHandler', xmlHttpRequest: XMLHttpRequest, info: imageInputInformation, core: Core): void;
-//   (event: 'videoUploadHandler', xmlHttpRequest: XMLHttpRequest, info: videoInputInformation, core: Core): void;
-//   (event: 'audioUploadHandler', xmlHttpRequest: XMLHttpRequest, info: audioInputInformation, core: Core): void;
-//   (
-//     event: 'resizeEditor',
-//     height: number,
-//     core: Core,
-//     prevHeight: number,
-//     resizeObserverEntry: ResizeObserverEntry | null,
-//   ): {};
-//   (event: 'getSunEditorInstance', sunEditor: SunEditorCore): void;
-// }
+// TODO: waiting for enabling to move outside of SFC, until Vue 3.3 release
+export interface IEmits {
+  (event: 'change', content: string): void;
+  (event: 'input', inputEvent: InputEvent): void;
+  (event: 'scroll', uiEvent: UIEvent): void;
+  (event: 'copy', clipboardEvent: ClipboardEvent, clipboardData: any): boolean;
+  (event: 'cut', clipboardEvent: ClipboardEvent, clipboardData: any): boolean;
+  (event: 'click', mouseEvent: PointerEvent): void;
+  (event: 'mouseDown', mouseEvent: MouseEvent): void;
+  (event: 'keyUp', keyboardEvent: KeyboardEvent): void;
+  (event: 'keyDown', keyboardEvent: KeyboardEvent): void;
+  (event: 'focus', focusEvent: FocusEvent): void;
+  (event: 'blur', focusEvent: FocusEvent): void;
+  (event: 'save', contents: string): void;
+  (event: 'setToolbarButtons', buttonList: Array<any>): void;
+  (event: 'load', reload: boolean): void;
+  (event: 'drop', dragEvent: DragEvent, cleanData: string, maxCharCount: boolean, core: Core): boolean | string;
+  (event: 'paste', clipboardEvent: ClipboardEvent, cleanData: string, maxCharCount: boolean, core: Core):
+    | boolean
+    | string;
+  (
+    event: 'imageUpload',
+    targetImgElement: HTMLImageElement,
+    index: number,
+    state: UploadStateType,
+    info: fileInfo,
+    remainingFilesCount: number,
+  ): void;
+  (
+    event: 'videoUpload',
+    targetElement: HTMLElement,
+    index: number,
+    state: UploadStateType,
+    info: fileInfo,
+    remainingFilesCount: number,
+  ): void;
+  (
+    event: 'audioUpload',
+    targetElement: HTMLElement,
+    index: number,
+    state: UploadStateType,
+    info: fileInfo,
+    remainingFilesCount: number,
+  ): void;
+  (
+    event: 'imageUploadBefore',
+    files: Array<File>, // TODO: different with origin
+    info: imageInputInformation,
+    core: Core,
+    uploadHandler: Function,
+  ): boolean | any[] | undefined;
+  (
+    event: 'videoUploadBefore',
+    files: Array<File>, // TODO: different with origin
+    info: videoInputInformation,
+    core: Core,
+    uploadHandler: Function,
+  ): boolean | any[] | undefined;
+  (
+    event: 'audioUploadBefore',
+    files: Array<File>, // TODO: different with origin
+    info: audioInputInformation,
+    uploadHandler: Function,
+  ): boolean | any[] | undefined;
+  (event: 'imageUploadError', errorMessage: string, result: any): boolean;
+  (event: 'videoUploadError', errorMessage: string, result: any): boolean;
+  (event: 'audioUploadError', errorMessage: string, result: any): boolean;
+  (event: 'toggleCodeView', isCodeView: boolean): void;
+  (event: 'toggleFullScreen', isFullScreen: boolean): void;
+  (event: 'showInline', context: Context, toolbar: Element): void;
+  (event: 'showController', controllers: Controllers, name: string): void;
+  (event: 'imageUploadHandler', xmlHttpRequest: XMLHttpRequest, info: imageInputInformation, core: Core): void;
+  (event: 'videoUploadHandler', xmlHttpRequest: XMLHttpRequest, info: videoInputInformation, core: Core): void;
+  (event: 'audioUploadHandler', xmlHttpRequest: XMLHttpRequest, info: audioInputInformation, core: Core): void;
+  (
+    event: 'resizeEditor',
+    height: number,
+    core: Core,
+    prevHeight: number,
+    resizeObserverEntry: ResizeObserverEntry | null,
+  ): {};
+  (event: 'getSunEditorInstance', sunEditor: SunEditorCore): void;
+}
 
-// export interface IExpose {
-//   getCharCount: (charCounterType?: string) => number;
-//   getContents: (onlyContents: boolean) => string;
-//   getContext: () => Context;
-//   getFilesInfo: (pluginName: string) => fileInfo[];
-//   getImagesInfo: () => fileInfo[];
-//   getText: () => string;
-//   insertHTML: (
-//     html: Element | string,
-//     notCleaningData?: boolean,
-//     checkCharCount?: boolean,
-//     rangeSelection?: boolean,
-//   ) => void;
-//   insertImage: (files: FileList) => void;
-//   save: () => void;
-// }
+// TODO: waiting for enabling to move outside of SFC, until Vue 3.3 release
+export interface IExpose {
+  appendContents: (contents: string) => void;
+  getCharCount: (charCounterType?: string) => number;
+  getContents: (onlyContents: boolean) => string;
+  getContext: () => Context;
+  getFilesInfo: (pluginName: string) => fileInfo[];
+  getImagesInfo: () => fileInfo[];
+  getText: () => string;
+  insertHTML: (
+    html: Element | string,
+    notCleaningData?: boolean,
+    checkCharCount?: boolean,
+    rangeSelection?: boolean,
+  ) => void;
+  insertImage: (files: FileList) => void;
+  save: () => void;
+  setContents: (contents: string) => void;
+}
+
+export interface ExportIEmits {
+  audioUpload: (
+    targetElement: HTMLElement,
+    index: number,
+    state: UploadStateType,
+    info: fileInfo,
+    remainingFilesCount: number,
+  ) => void;
+  audioUploadBefore: (
+    files: Array<File>, // TODO: different with origin
+    info: audioInputInformation,
+    uploadHandler: Function,
+  ) => boolean | any[] | undefined;
+  audioUploadError: (errorMessage: string, result: any) => boolean;
+  audioUploadHandler: (xmlHttpRequest: XMLHttpRequest, info: audioInputInformation, core: Core) => void;
+  blur: (focusEvent: FocusEvent) => void;
+  change: (content: string) => void;
+  click: (mouseEvent: PointerEvent) => void;
+  copy: (clipboardEvent: ClipboardEvent, clipboardData: any) => boolean;
+  cut: (clipboardEvent: ClipboardEvent, clipboardData: any) => boolean;
+  drop: (dragEvent: DragEvent, cleanData: string, maxCharCount: boolean, core: Core) => boolean | string;
+  focus: (focusEvent: FocusEvent) => void;
+  getSunEditorInstance: (sunEditor: SunEditorCore) => void;
+  imageUpload: (
+    targetImgElement: HTMLImageElement,
+    index: number,
+    state: UploadStateType,
+    info: fileInfo,
+    remainingFilesCount: number,
+  ) => void;
+  imageUploadBefore: (
+    files: Array<File>, // TODO: different with origin
+    info: imageInputInformation,
+    core: Core,
+    uploadHandler: Function,
+  ) => boolean | any[] | undefined;
+  imageUploadError: (errorMessage: string, result: any) => boolean;
+  imageUploadHandler: (xmlHttpRequest: XMLHttpRequest, info: imageInputInformation, core: Core) => void;
+  input: (inputEvent: InputEvent) => void;
+  keyDown: (keyboardEvent: KeyboardEvent) => void;
+  keyUp: (keyboardEvent: KeyboardEvent) => void;
+  load: (reload: boolean) => void;
+  mouseDown: (mouseEvent: MouseEvent) => void;
+  paste: (clipboardEvent: ClipboardEvent, cleanData: string, maxCharCount: boolean, core: Core) => boolean | string;
+  resizeEditor: (height: number, core: Core, prevHeight: number, resizeObserverEntry: ResizeObserverEntry | null) => {};
+  save: (contents: string) => void;
+  scroll: (uiEvent: UIEvent) => void;
+  setToolbarButtons: (buttonList: Array<any>) => void;
+  showController: (controllers: Controllers, name: string) => void;
+  showInline: (context: Context, toolbar: Element) => void;
+  toggleCodeView: (isCodeView: boolean) => void;
+  toggleFullScreen: (isFullScreen: boolean) => void;
+  videoUpload: (
+    targetElement: HTMLElement,
+    index: number,
+    state: UploadStateType,
+    info: fileInfo,
+    remainingFilesCount: number,
+  ) => void;
+  videoUploadBefore: (
+    files: Array<File>, // TODO: different with origin
+    info: videoInputInformation,
+    core: Core,
+    uploadHandler: Function,
+  ) => boolean | any[] | undefined;
+  videoUploadError: (errorMessage: string, result: any) => boolean;
+  videoUploadHandler: (xmlHttpRequest: XMLHttpRequest, info: videoInputInformation, core: Core) => void;
+}
 
 type DefineEmitFuncBase = (event: string, ...args: any[]) => any;
 type ExportEmitFuncBase = (...args: any[]) => any;
