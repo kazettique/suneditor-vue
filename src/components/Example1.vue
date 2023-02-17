ea9d7c1fec5db63a2949fc1410e8bb450c3981db
Woody Inuyama <kazettique@gmail.com>
2023-02-17 08:06:07 +0800

    update example comp

--- src/components/Example1.vue
+++ src/components/Example1.vue
@@ -47,22 +47,22 @@ import type { ExportIEmits, IExpose, SetOptions } from '@/types';
 
 const editorEl = ref<InstanceType<typeof SunEditor> | null>(null);
 
-const editorConfig: SetOptions = {
+const setOptions: SetOptions = {
   buttonList: [
     ['undo', 'redo'],
-    // ['font', 'fontSize', 'formatBlock'],
-    // ['paragraphStyle', 'blockquote'],
-    // ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
-    // ['fontColor', 'hiliteColor', 'textStyle'],
-    // ['removeFormat'],
-    // '/', // Line break
-    // ['outdent', 'indent'],
-    // ['align', 'horizontalRule', 'list', 'lineHeight'],
-    // ['table', 'link', 'image', 'video', 'audio' /** ,'math' */], // You must add the 'katex' library at options to use the 'math' plugin.
+    ['font', 'fontSize', 'formatBlock'],
+    ['paragraphStyle', 'blockquote'],
+    ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
+    ['fontColor', 'hiliteColor', 'textStyle'],
+    ['removeFormat'],
+    '/', // Line break
+    ['outdent', 'indent'],
+    ['align', 'horizontalRule', 'list', 'lineHeight'],
+    ['table', 'link', 'image', 'video', 'audio' /** ,'math' */], // You must add the 'katex' library at options to use the 'math' plugin.
     /** ['imageGallery'] */ // You must add the "imageGalleryUrl".
-    // ['fullScreen', 'showBlocks', 'codeView'],
-    // ['preview', 'print'],
-    // ['save', 'template'],
+    ['fullScreen', 'showBlocks', 'codeView'],
+    ['preview', 'print'],
+    ['save', 'template'],
     /** ['dir', 'dir_ltr', 'dir_rtl'] */ // "dir": Toggle text direction, "dir_ltr": Right to Left, "dir_rtl": Left to Right
   ],
   charCounter: true,
@@ -92,7 +92,7 @@ const editorProps: IProps = {
   setAllPlugins: false,
   setContents: 'test set contents', // TODO: no need?
   setDefaultStyle: 'test set default style',
-  setOptions: editorConfig,
+  setOptions,
 };
 
 // * EVENTS
